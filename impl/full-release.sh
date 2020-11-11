10dc5d6ad634d28b1f365a8a7aa65bcb443b1010
Robb Matzke <matzke1@llnl.gov>
2020-11-11 17:52:24 -0500

    Adding support for more full releases (WIP)

--- impl/full-release.sh
+++ impl/full-release.sh
@@ -1,6 +1,6 @@
 ########################################################################################################################
-# This file provides functions that are needed when installing JOVIAL analysis on various platforms. This functions
-# defined in this file override the base class implementations in base-class.sh, and in turn are overridden by
+# This file provides functions that are needed when installing a full release of ROSE on various platforms. This
+# functions defined in this file override the base class implementations in base-class.sh, and in turn are overridden by
 # subclasses.
 ########################################################################################################################
 
@@ -15,19 +15,11 @@ choose-rose-dependencies() {
     (
         set -x
 
-        # patchelf-0.10 doesn't compile here, but 0.9 does.
-        run spock-shell --with patchelf-0.9 --install true
-
-        # Customer requires GCC 6.1.0 (gnu++14 is the default language for this C++ compiler).
-        #
-        # Customer requires Boost 1.61.
-        #
         # For some reason, Jovial analysis is enabled unlike any other analysis. Instead of adding "jovial" to the list
         # of supported languages, there's an entirely separate switch for it. Unfortunately, RMC/Spock doesn't know
         # about this nonstandard way of enabling a language, so we need to use the catch-all "rmc_other". Also, beware
         # of the mixed-style name containing both hyphens and underscores.
-        #
-        (cd rose/_build && run env COMPILER="gnu-gnu++14-6.1.0" LANGUAGES="c,c++" BOOST=1.63 JQ=none rmc init --batch ..)
+        (cd rose/_build && run env LANGUAGES="binaries,c,c++" rmc init --batch ..)
         local stratego_root="$(pwd)/rose/_build/stratego"
         echo "rmc_other '--enable-experimental_jovial_frontend --with-aterm=$stratego_root --with-stratego=$stratego_root'" >>rose/_build/.rmc-main.cfg
     )
@@ -62,9 +54,9 @@ install-rose-dependencies() {
     # serially. (Update: parallel build failed with "/bin/sh: line 3: /rose/_build/strategoxt-0.17.1/xtc/src/xtc: No
     # such file or directory". This might be unrelated to a parallel build, but I'm disabling parallelism temporarily to
     # see if it fixes it.)
-    run rmc -C rose/_build bash -c '
+    cat >build-jovial-dependencies <<'EOF'
         set -ex
-        export STRATEGO_HOME=\$RG_BLD/stratego
+        export STRATEGO_HOME=$RG_BLD/stratego
         export CFLAGS=-DAT_64BIT
 
         # Temporarily disabling parallelism. See comment above.
@@ -78,36 +70,37 @@ install-rose-dependencies() {
         # The 64-bit ATerm library
         tar xf /software/aterm-3.0.tar.gz
         cd aterm-3.0/aterm
-        ./configure --prefix=\$STRATEGO_HOME
-        #make -j\$RMC_PARALLELISM
+        ./configure --prefix=$STRATEGO_HOME
+        #make -j$RMC_PARALLELISM
         make install
-        mkdir -p \$STRATEGO_HOME/lib/pkgconfig
-        sed \"s/\(^Cflags:.*\)/\1 -DAT_64BIT/\" <aterm.pc >\$STRATEGO_HOME/lib/pkgconfig/aterm.pc
-        cd \$RG_BLD
+        mkdir -p $STRATEGO_HOME/lib/pkgconfig
+        sed "s/\(^Cflags:.*\)/\1 -DAT_64BIT/" <aterm.pc >$STRATEGO_HOME/lib/pkgconfig/aterm.pc
+        cd $RG_BLD
 
         # The sdf2-bundle
         tar xf /software/sdf2-bundle-2.4.1.tar.gz
         cd sdf2-bundle-2.4.1
-        ./configure --with-aterm=\$STRATEGO_HOME --prefix=\$STRATEGO_HOME
-        make -j\$RMC_PARALLELISM
+        ./configure --with-aterm=$STRATEGO_HOME --prefix=$STRATEGO_HOME
+        make -j$RMC_PARALLELISM
         make install    
-        cd \$RG_BLD
+        cd $RG_BLD
 
         # Strategoxt
         tar xf /software/strategoxt-0.17.1.tar.gz
         cd strategoxt-0.17.1
-        ./configure --with-aterm=\$STRATEGO_HOME --with-sdf=\$STRATEGO_HOME --prefix=\$STRATEGO_HOME
-        make -j\$RMC_PARALLELISM
+        ./configure --with-aterm=$STRATEGO_HOME --with-sdf=$STRATEGO_HOME --prefix=$STRATEGO_HOME
+        make -j$RMC_PARALLELISM
         make install
-        cd \$RG_BLD
+        cd $RG_BLD
 
         # Test
-        #export PATH=\"\$STRATEGO_HOME/bin:\$PATH\"
-        #sglri -p \$RG_SRC/src/3rdPartyLibraries/experimental-jovial-parser/share/rose/Jovial.tbl -i jovial_file.jov
+        #export PATH="$STRATEGO_HOME/bin:$PATH"
+        #sglri -p $RG_SRC/src/3rdPartyLibraries/experimental-jovial-parser/share/rose/Jovial.tbl -i jovial_file.jov
 
         # Clean up source and build trees in order to not accidentally use something that we later do not distribute
         rm -rf aterm-3.0 ._aterm-3.0 sdf2-bundle-2.4.1 strategoxt-0.17.1
-        '
+EOF
+    run rmc -C rose/_build bash build-jovial-dependencies
 }
 
 [[ override ]]
